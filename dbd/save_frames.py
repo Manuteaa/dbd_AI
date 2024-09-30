76316c06795c0c6758e3bbaf1cabb1cbbbf374f5
Nicolas Beltrand <nicolas.beltrand@yahoo.fr>
2024-09-30 09:25:11 +0200

    save 1080x1080 frames then resize to 380x380

--- dbd/save_frames.py
+++ dbd/save_frames.py
@@ -1,27 +1,31 @@
-import numpy as np
-import cv2
+import pathlib
 import mss
 import time
 import os
+
+from PIL import Image
 
 from dbd.utils.frame_grabber import get_monitor_attributes
 
 
 if __name__ == '__main__':
     # Make new dataset folder, where we save the frames
+    pathlib.Path("dataset").mkdir(exist_ok=True)
     timestr = time.strftime("%Y%m%d-%H%M%S")
     dataset_folder = os.path.join("dataset", timestr)
-    os.mkdir(dataset_folder)
+    pathlib.Path(dataset_folder).mkdir(exist_ok=True)
 
     # Get monitor attributes
     monitor = get_monitor_attributes()
 
     with mss.mss() as sct:
+        print("Saving images in {}".format(dataset_folder))
         i = 0
 
         # Infinite loop
         while True:
-            screenshot = np.array(sct.grab(monitor))
-            # screenshot = cv2.resize(screenshot)  # Resize to 420x420, experiment strategies not to add too much distortion to the original non-squared images
-            cv2.imwrite(os.path.join(dataset_folder, "{}_{}.png".format(timestr, i)), screenshot)
+            screenshot = sct.grab(monitor)
+            image_pil = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "BGRX")
+            image_pil = image_pil.resize((380, 380), resample=Image.LANCZOS)
+            image_pil.save(os.path.join(dataset_folder, "{}_{}.png".format(timestr, i)))
             i += 1
--- dbd/utils/frame_grabber.py
+++ dbd/utils/frame_grabber.py
@@ -5,10 +5,11 @@ def get_monitor_attributes():
     """Get monitor attributes for training
     Save entire main screen"""
     width, height = pyautogui.size()
-    monitor = {"top": 0,
-               "left": 0,
-               "width": width,
-               "height": height}
+    object_size = 1080
+    monitor = {"top": height // 2 - object_size // 2,
+               "left": width // 2 - object_size // 2,
+               "width": object_size,
+               "height": object_size}
 
     return monitor
 
@@ -17,10 +18,18 @@ def get_monitor_attributes_test():
     """ Get monitor attributes for inference
     Save entire main screen
     """
+
     width, height = pyautogui.size()
     monitor = {"top": 0,
                "left": 0,
                "width": width,
                "height": height}
+
+    # width, height = pyautogui.size()
+    # object_size = 224
+    # monitor = {"top": height // 2 - object_size // 2,
+    #            "left": width // 2 - object_size // 2,
+    #            "width": object_size,
+    #            "height": object_size}
 
     return monitor
