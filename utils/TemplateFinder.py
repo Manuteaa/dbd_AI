9080eeb6ef68d7baf1f73c226e16cf37e9cfccbf
Nicolas Beltrand <nicolas.beltrand@yahoo.fr>
2024-10-05 18:17:52 +0200

    templatefinder : fix shape error, fix possible seg-fault when cropping

--- utils/TemplateFinder.py
+++ utils/TemplateFinder.py
@@ -1,6 +1,4 @@
 import cv2
-import numpy as np
-from PIL import Image
 
 
 class TemplateFinder:
@@ -10,9 +8,10 @@ class TemplateFinder:
     def find_skill_check(self, frame_grayscale, threshold=0.4):
         res = cv2.matchTemplate(frame_grayscale, self.template, cv2.TM_CCOEFF_NORMED)
         _, max_val, _, max_loc = cv2.minMaxLoc(res)
+        # print(max_val)
 
         if max_val > threshold:
-            w, h = self.template.shape
+            h, w = self.template.shape
             top_left = max_loc
             bottom_right = (top_left[0] + w, top_left[1] + h)
             return top_left, bottom_right
@@ -24,9 +23,24 @@ class TemplateFinder:
         return frame_grayscale
 
     def crop_frame_from_location(self, frame, object_size, top_left, bottom_right):
+        h, w = frame.shape[:2]
         center_x = (top_left[0] + bottom_right[0]) // 2  # col
         center_y = (top_left[1] + bottom_right[1]) // 2  # row
-
         demi_object_size = object_size // 2
-        crop = frame[center_y - demi_object_size:center_y + demi_object_size, center_x - demi_object_size:center_x + demi_object_size]
+
+        start_x = max(0, center_x - demi_object_size)
+        start_y = max(0, center_y - demi_object_size)
+        end_x = min(h, center_x + demi_object_size)
+        # end_y = min(w, center_y + demi_object_size)
+        end_y = start_y + 223
+
+        padding_x = object_size - (end_x - start_x)
+        padding_y = object_size - (end_y - start_y)
+        padding_x_y = max(padding_x, padding_y)
+
+        # To avoid seg-fault
+        if padding_x_y != 0:
+            frame = cv2.copyMakeBorder(frame, padding_x_y, padding_x_y, padding_x_y, padding_x_y, cv2.BORDER_REFLECT)
+
+        crop = frame[start_y:end_y, start_x:end_x]
         return crop
